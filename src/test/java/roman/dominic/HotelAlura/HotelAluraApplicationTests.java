ddac4b0fab195f41b563b775f90135e2780e3e59
Dominic Roman <romandominic30@gmail.com>
2023-08-23 14:28:22 -0500

    first commit

---
+++ src/test/java/roman/dominic/HotelAlura/HotelAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package roman.dominic.HotelAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
